89e6d0e7344115e2fd4dce39de89b5eeaf5cf862
Jackson <jackson.bounds@gmail.com>
2022-08-21 19:01:27 -0400

    Works

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,68 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("aabbaa"));
+        String str = longestPalindrome("cbbd");
+        System.out.println("done");
     }
-    public static boolean isPalindrome(String s) {
-        Stack<Character> stack = new Stack<>();
-        int midPoint = s.length() / 2;
+    public static String longestPalindrome(String s) {
+        String largest = "";
+        for (int i = 0; i < s.length(); i++) {
+            String current = expandOutwards(s, i);
+            if (current.length() > largest.length()) {
+                largest = current;
+            }
+        }
+          return largest;
+    }
 
-        // Add the first half of the string
-        for (int i = 0; i < midPoint; i++) {
-            stack.push(s.charAt(i));
+    public static String expandOutwards(String s, int startIndex) {
+        boolean oddLooped = false;
+        int leftIndexOdd = startIndex - 1;
+        int rightIndexOdd = startIndex + 1;
+        while ((leftIndexOdd >= 0) && (rightIndexOdd <= (s.length() - 1))) {
+            oddLooped = true;
+            char leftChar = s.charAt(leftIndexOdd);
+            char rightChar = s.charAt(rightIndexOdd);
+            if (leftChar != rightChar) {
+                break;
+            }
+            leftIndexOdd--;
+            rightIndexOdd++;
+        }
+        leftIndexOdd++;
+        rightIndexOdd--;
+        // If oddLooped isn't true, we are an edge, must be the startIndex character
+
+        boolean evenLooped = false;
+        int leftIndexEven = startIndex;
+        int rightIndexEven = startIndex + 1;
+        while ((leftIndexEven >= 0) && (rightIndexEven <= (s.length() - 1))) {
+            char leftChar = s.charAt(leftIndexEven);
+            char rightChar = s.charAt(rightIndexEven);
+            if (leftChar != rightChar) {
+                break;
+            }
+            evenLooped = true;
+            leftIndexEven--;
+            rightIndexEven++;
+
+        }
+        leftIndexEven++;
+        rightIndexEven--;
+        // If evenLooped isn't true, we must be on the far right character, return that character
+
+        String oddString = Character.toString(s.charAt(startIndex));
+        String evenString = Character.toString(s.charAt(startIndex));
+        if (oddLooped) {
+            oddString = s.substring(leftIndexOdd, rightIndexOdd + 1);
+        }
+        if (evenLooped) {
+            evenString = s.substring(leftIndexEven, rightIndexEven + 1);
         }
 
-        if (s.length() % 2 == 1) {
-            midPoint += 1;
-        }
-
-        for (int i = midPoint; i < s.length(); i++) {
-            char toCompare = stack.pop();
-            if (toCompare != s.charAt(i)) return false;
-        }
-        return true;
+        if (oddString.length() >= evenString.length()) {
+            return oddString;
+        } else return evenString;
     }
 
 }
